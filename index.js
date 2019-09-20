676eb0a8823b7968b80c397f22958e5f9f736b10
george-kirby <georgeskirby@btinternet.com>
2019-09-20 14:53:51 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(data => data.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
